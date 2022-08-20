64c3e1dd225ec716067da2ae4c17b0d7961924d3
32-Mimhakkani <mim.cse32@gmail.com>
2022-08-20 18:34:02 +0600

    pass object data using components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import FirstJsxPratices from './Components/FirstJsx';
 import PersonalInfo from './Components/PersonalInfo';
+import PassOobjectDataProps from './Components/PassOobjectDataProps';
 
 
 function App() {
+  const objectData = {
+    salary : 40000,
+    petName : 'Ovi',
+    color :'black'
+  }
   return (
  <div>
   <FirstJsxPratices />
@@ -12,6 +18,9 @@ function App() {
   <PersonalInfo />
   <PersonalInfo name="Hakkani" location="Rajshahi" designation="Web Designer"/>
   <PersonalInfo name="Brain Station" location="Dhaka" designation="All Developer"/>
+  
+  <PassOobjectDataProps objectData={objectData}/>
+
  </div>
   );
 }
---
+++ src/Components/PassOobjectDataProps.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const PassOobjectDataProps = ({objectData}) => {
+
+    console.log('see the object data',objectData);
+    return (
+        <div style={{color:'#548758'}}>
+            <p>Salary :{objectData.salary}</p>
+            <p>PetName :{objectData.petName}</p>
+            <p>Favoroute Color :{objectData.color}</p>
+        </div>
+    );
+};
+
+export default PassOobjectDataProps;

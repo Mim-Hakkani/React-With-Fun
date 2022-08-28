3641b14774c51e524186bc40c3a6c6ef6293aa24
32-Mimhakkani <mim.cse32@gmail.com>
2022-08-28 11:53:37 +0600

    react lifecycle method

--- src/Components/ReacjHooks/EffectCounterDom.js
+++ src/Components/ReacjHooks/EffectCounterDom.js
@@ -15,7 +15,37 @@ const EffectCounterDom = () => {
        useEffect(()=>{
           document.getElementById("header").style.color="red"
              document.title =`Count ${count}`;
+             
           },[count])
+
+
+
+  const [count2, setCount2] = useState(0);
+
+  // componentDidMount
+  useEffect(() => {
+    console.log("The use effect ran");
+  }, []);
+
+  // // componentDidUpdate
+  useEffect(() => {
+    console.log("The use effect ran");
+  }, [count, count2]);
+
+  // componentWillUnmount
+  useEffect(() => {
+    console.log("The use effect ran");
+    return () => {
+      console.log("the return is being ran");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(`The count has updated to ${count}`);
+    return () => {
+      console.log(`we are in the cleanup - the count is ${count}`);
+    };
+  }, [count]);
 
 
     return (
